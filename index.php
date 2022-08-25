32e7350501f728f36375ae2c1105c33323a52a53
ROBSON ARRUDA LIMA <robson.alima1@senacsp.edu.br>
2022-08-24 22:06:43 -0300

    Robson Arruda

--- index.php
+++ index.php
@@ -1,7 +1,59 @@
 <?php
-$nome ='Robson Arruda';
+$diaSemana ='5';
 
-echo " Olá $nome";
+switch ($diaSemana) {
+    case 0:
+        echo "Domingo";
+        break;
+    case 1:
+        echo "Segunda";
+        break;
+    case 2:
+        echo "Terça";
+        break;
+    case 3:
+        echo "Quarta";
+        break;
+    case 4:
+        echo "Quinta";
+        break;
+    case 5:
+        echo "Sexta";
+        break;
+    case 6:
+        echo "Sabado";
+        break;
+    case 7:
+        echo "Domingo";
+         break;
+                                                
+}
+
+/*if (5 >10 ){
+    echo "O 5 é maior";
+    } 
+    else {
+        echo "O 5 NÃO é o maior";
+    };
+//echo " Olá $nome"; //variavel //interpolação
+/*
+$i = 1;
+while ($i <=10) {
+    echo "Robson Arruda <br>";
+    $i++;
+} 
+*/
+/*
+$i = 0;
+do {
+    echo "Robson Arruda <br>";
+    $i++;
+} while ($i<9   );
+
+/*
+for ($nome = 1; $nome <= 10; $nome++ ){
+    echo 'Robson Arruda <br>';
+}
 //comtario de linha
 #comentario de linha 
 /*
@@ -9,3 +61,4 @@ comentario
 de 
 bloco
 */
+//documentação dp PHP fica em: www.php.net
